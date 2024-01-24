3352e22e3038879f661ed851692d9c7c04892abb
Serhii <sergejkovalkykc777@gmail.com>
2024-01-24 20:58:56 +0200

    bonus

---
+++ lesson 7/lesson/lesson 7.4.py
@@ -0,0 +1,384 @@
+print('Привествую вас на векторине по вселеной игры Portal!')
+score = 0
+name = (input('Введите свое имя: '))
+print('Прекрасное имя!')
+print('Первый вопрос:')
+print('В каком году вышла игра Portal?')
+print('1-10 октября 2007')
+print('2-10 октября 2008')
+print('3-21 апреля 2011')
+print('4-16 марта 2009')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+1
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Второй вопрос:')
+print('Как называеться кнопка на которую нужно положить куб?')
+print('1-экспериментальная кнопка')
+print('2-сверхкнопка')
+print('3-300-мегаваттная экспериментальная сверхпрочная сверхударная сверхкнопка')
+print('4-1500-мегаваттная экспериментальная сверхпрочная сверхударная сверхкнопка')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 1
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 1
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Третий вопрос:')
+print('Сколько всего тестовых камер в игре Portal?')
+print('1-20')
+print('2-19')
+print('3-15')
+print('4-18')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+0
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 1
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Четвертый вопрос:')
+print('Какая компания выпустила Portal?')
+print('1-Valve')
+print('2-Ubisoft')
+print('3-EA Games')
+print('4-Microsoft')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+1
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 1
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Пятый вопрос:')
+print('На каком движке была создана Portal?')
+print('1-Cocos 20')
+print('2-Source 2')
+print('3-Source')
+print('4-Impact')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+0
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 1
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Шестой вопрос:')
+print('Какого цвета порталы?')
+print('1-Синий и фиолетовый')
+print('2-Желтый и оранжевый')
+print('3-Красный и зеленый')
+print('4-Синий и оранжевый')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 1
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+0
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 1
+        print('Ответа нету')
+print('Седьмой вопрос:')
+print('Кто локализовал на руский язык игру Portal?')
+print('1-Бука')
+print('2-Бобры')
+print('3-Valve')
+print('4-Команда Игоря')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+1
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Восьмой вопрос:')
+print('Как зовут главную героиню?')
+print('1-Анастасия')
+print('2-Гладос')
+print('3-Уитли')
+print('4-Челл')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 1
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+0
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 1
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Девятый вопрос:')
+print('Сколько стоит Portal в Steam?')
+print('1-229грн')
+print('2-300грн')
+print('3-79грн')
+print('4-69грн')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+1
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Десятый вопрос:')
+print('Торт это...')
+print('1-Сладкое кондитерское изделие из сдобного теста с кремом, фруктами и т. п')
+print('2-Правда')
+print('3-Ложь')
+print('4-Торт')
+choice = input('Выберете вариант ответа:')
+if choice == '1':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '2':
+    score = score + 0
+    print('Ответ засчитан')
+elif choice == '3':
+    score = score + 1
+    print('Ответ засчитан')
+elif choice == '4':
+    score = score + 0
+    print('Ответ засчитан')
+else:
+    print('Недопустимый вариант ответа')
+    choice = input('Выберете последний вариант ответа:')
+    if choice == '1':
+        score = score+0
+        print('Ответ засчитан')
+    elif choice == '2':
+        score = score + 0
+        print('Ответ засчитан')
+    elif choice == '3':
+        score = score + 1
+        print('Ответ засчитан')
+    elif choice == '4':
+        score = score + 0
+        print('Ответ засчитан')
+    else:
+        score = score + 0
+        print('Ответа нету')
+print('Поздравляю вы прошли этот "очень сложный" тест')
+print('Ваш результат:', score,'/10')
+if score == 0:
+    print('Поиграй в Portal')
+elif 1 <= score <= 5:
+    print('Молодец,но ты не особо связан с этой игрой')
+elif 6 <= score <= 9:
+    print('Ты хорошо знаешь эту вселеную')
+else:
+    print('*Вам пришло приглашение в Aperture Science*')
