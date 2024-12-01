c47eb66880174a1bedaad6a2541c1843627327c7
StrakFel <154830897+StrakFel@users.noreply.github.com>
2024-12-01 17:00:06 +0300

    Add files via upload

---
+++ c++/ConsoleApplication18/ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+    cout << "Enter word: ";
+    cin >> word;
+
+    string reversed_word;
+    for (int i = word.size() - 1; i >= 0; --i) {
+        reversed_word += word[i];
+    }
+
+    ofstream file("words.txt", ios::app);
+    if (file.is_open()) {
+        file << reversed_word << "\n";
+        file.close();
+        cout << "Word \"" << reversed_word << "\" written to file words.txt.\n";
+    }
+    else {
+        cerr << "Could not open file for writing\n";
+    }
+
+    return 0;
+}
