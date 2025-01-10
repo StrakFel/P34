fe8f4cb51f562fdfc08720506deaab84bc8895b1
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-01-10 20:12:06 +0200

    Add files via upload

---
+++ C#/lesson 1/lesson 1.1/lesson 1.1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите размер массива: ");
+            int array_size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[array_size];
+
+            Console.WriteLine("\nНапишите целые числа, которые будут находиться в массиве:");
+            for (int i = 0; i < array_size; i++) 
+            {
+                Console.Write($"Введите {i+1} элемент: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Введите число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n1.Ваш массив:");
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            
+            //int minValue = array.Min();
+            //int maxValue = array.Max();
+
+            int min_number = array[0];
+            int max_number = array[0];
+
+            for (int i = 0; i < array_size; i++)
+            {
+                if (array[i] < min_number){
+                    min_number = array[i];
+                }
+                if (array[i] > max_number){
+                    max_number = array[i];
+                }
+            }
+            Console.WriteLine($"\n\n2.Минимальное число - {min_number}\n  Максимальное число - {max_number}\n");
+
+            int sum_array = 0;
+            for (int i = 0; i < array_size; i++)
+            {
+                sum_array += array[i];
+            }
+            Console.WriteLine($"\n3.Сумма всех элементов массива: {sum_array}");
+
+            int mean_array = sum_array / array_size;
+            Console.WriteLine($"\n4.Среднее арифметическое массива: {mean_array}");
+
+            int comparison_array_x = 0;
+            for (int i = 0; i < array_size; i++)
+            {
+                if (array[i] > x){
+                    comparison_array_x += 1;
+                }
+            }
+            Console.WriteLine($"\nКоличество чисел больше за ваше число {x}: {comparison_array_x}");
+        }
+    }
+}
---
+++ C#/lesson 1/lesson 1/lesson 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Целые числа
+            int a1 = 20;
+            long a2 = -20;
+            byte a3 = 100;
+            //Дробные числа
+            double a4 = 2.2;
+            float a5 = -100.100f;
+            decimal a6 = 123.456m;
+            //Другие типы
+            bool a7 = true;
+            char a8 = 'a';
+            string a9 = "Hello";
+
+            object a10 = null;
+
+            Console.WriteLine("The first lesson on C#");//Метод для вывода (как print или cout)
+            Console.Beep();//Метод, который издает звук
+
+            int number  = Convert.ToInt32(Console.ReadLine());//Преобразует ReadLine(то есть string) в int
+            Console.WriteLine(number * number);
+            Console.ReadLine();//Метод, который считывает введенный пользователем текст
+
+            int[] numbers;//Инициализация массива
+            numbers = new int[10] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};//Заполнение массива
+
+            //Циклы с одномерным массивом
+            for (int i = 0; i < numbers.Length; i++)//Перебирает индексы массива с 0 до его длины(numbers.Length)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (int n in numbers)//Перебирает значение элементов массива 
+            {
+                Console.WriteLine(n);
+            }
+
+            int[][] numbers_2 = new int[10][];//Двумерный и ступенчатый массивы
+
+            //Двухмерный массив
+            int[,] numbers_3 =
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+            };
+        }
+    }
+}
