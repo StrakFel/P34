eb526d131559554fad98c0b3184f806a00b5d9fd
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-01-12 11:28:14 +0200

    Add files via upload

---
+++ C#/lesson 3/lesson 3/lesson 3/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson_3
+{
+    internal class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void Say_Hello()
+        {
+            Console.WriteLine($"Hi, my name is {name}. I`m {age} years old.");
+        }
+    }
+}
---
+++ C#/lesson 3/lesson 3/lesson 3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Person person_1 = new Person { name = "Serhii", age = 15 };
+            //person_1.Say_Hello();
+            Person person_2 = new Person ("Vladislav",19);
+            person_2.Say_Hello();
+            person_2.age = 1;
+            person_2.Say_Hello();
+
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person());
+
+
+            int num = 0;
+            try
+            {
+                Console.WriteLine("Enter the number");
+                //int num = Convert.ToInt32(Console.ReadLine());
+                num = int.Parse(Console.ReadLine());
+                int a = 20, b = 0;
+                Console.WriteLine(a / b);
+            }
+            catch (FormatException ex) //Тип ошибки, название
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+            }
+            finally
+            {
+                Console.WriteLine($"Your number is {num}");
+            }
+
+            //try - код, который нужно проверить
+            //catch - обработка исключения
+            //finally - выполняет код в независимости от того была ли ошибка
+            //throw - используется для генерации исключений
+        }
+    }
+}
