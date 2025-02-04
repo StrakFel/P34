bc5f01f5c8d3cf97a7602c112afe49ef92277868
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-02-04 20:29:44 +0200

    Add files via upload

---
+++ C#/lesson 6/Hw/Hw 6.1/Hw 6.1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw_6._1
+{
+    class MultyMatrix
+    {
+        private double[,] data;
+
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public MultyMatrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            data = new double[rows, columns];
+        }
+
+        public double this[int row, int col]
+        {
+            get { return data[row, col]; }
+            set { data[row, col] = value; }
+        }
+
+        public void FillRandom()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    data[i, j] = rand.NextDouble() * 100;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write($"{data[i, j]:F2}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            MultyMatrix matrix = new MultyMatrix(3, 4);
+            matrix.FillRandom();
+            matrix.Print();
+        }
+    }
+}
