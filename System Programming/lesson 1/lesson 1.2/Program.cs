52579932b659c46ba674ff3a110a0ea184a50086
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-04-26 10:43:06 +0300

    Add files via upload

---
+++ System Programming/lesson 1/lesson 1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lesson_1._2
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowText(IntPtr a, string lpString);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter programm`s name: ");
+            string newProgramName = Console.ReadLine();
+            IntPtr hWnd = GetForegroundWindow();
+            SetWindowText(hWnd, newProgramName);
+            Console.ReadLine();
+        }
+    }
+}
