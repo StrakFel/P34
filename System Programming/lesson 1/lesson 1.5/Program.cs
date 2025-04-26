27fcdbbfff6e8101f25c663b3d6760ad7a94992a
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-04-26 10:44:12 +0300

    Add files via upload

---
+++ System Programming/lesson 1/lesson 1.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lesson_1._5
+{
+    internal class Program
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        public static extern void ShellExecute(IntPtr hwnd, string lpOperation, string lpFile, string lpParametrs, string lpDirectory, int nShowCmd);
+
+        static void Main(string[] args)
+        {
+            ShellExecute(IntPtr.Zero, "open", "steam.exe", null, "C:\\Program Files (x86)\\Steam", 1);
+
+            Console.ReadLine();
+        }
+    }
+}
