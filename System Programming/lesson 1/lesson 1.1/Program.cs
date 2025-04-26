12a7d6b0021cd6c2800e41435b5277730c7c070d
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-04-26 10:41:42 +0300

    Add files via upload

---
+++ System Programming/lesson 1/lesson 1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lesson_1._1
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", EntryPoint = "Beep")] //EntryPoint - точка вхождение в программу
+        public static extern bool SystemBeep(uint a, uint b);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string capture, uint type);  //int - потому что функция должна что то возвращать
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern void Sleep(uint dwMilliseconds);
+
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Привіт", "text", 0);
+            Sleep(1000);
+        }
+    }
+}
