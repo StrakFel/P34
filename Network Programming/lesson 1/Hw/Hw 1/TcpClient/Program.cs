672eef602c0d50fd3a9a6d9315c0311adbee2c08
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-04-27 10:27:33 +0300

    Add files via upload

---
+++ Network Programming/lesson 1/Hw/Hw 1/TcpClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcp_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Налаштування
+            int port = 5000;
+            string serverIp = "127.0.0.1";
+
+            try
+            {
+                TcpClient client = new TcpClient(serverIp, port);
+                NetworkStream stream = client.GetStream();
+
+                Console.Write("Enter your message: ");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Message sent");
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string responce = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"Server: {responce}");
+
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            Console.Write("Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Network Programming/lesson 1/Hw/Hw 1/TcpServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Tcp_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 5000;
+            IPAddress localAdress = IPAddress.Parse("127.0.0.1");
+
+            TcpListener server = new TcpListener(localAdress, port);
+            server.Start();
+            Console.WriteLine("Server start! Wait new client...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client online");
+
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Message: {data}");
+
+                string responce = $"{data.ToUpperInvariant()}";
+                byte[] responceBytes = Encoding.UTF8.GetBytes(responce);
+                stream.Write(responceBytes, 0, responceBytes.Length);
+
+                client.Close();
+                Console.WriteLine("Client offline");
+            }
+        }
+    }
+}
