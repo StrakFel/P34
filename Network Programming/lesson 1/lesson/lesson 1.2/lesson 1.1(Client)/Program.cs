af3c06ebe84d3af0da0eb46f87dcba457d7a4291
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-04-27 10:26:44 +0300

    Add files via upload

---
+++ Network Programming/lesson 1/lesson/lesson 1.2/lesson 1.1(Client)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_1_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Налаштування
+            int port = 5000;
+            string serverIp = "127.0.0.1";
+
+            try
+            {
+                //Створення клієнту
+                TcpClient client = new TcpClient(serverIp, port);
+                NetworkStream stream = client.GetStream();
+
+                //Відправка повідомлення
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(name);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Your name sent");
+
+                //Отримання відповіді
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string responce = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"Server: {responce}");
+
+                //Завершення
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Network Programming/lesson 1/lesson/lesson 1.2/lesson 1.1(Server)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lesson1_1_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Налаштування
+            int port = 5000;
+            IPAddress localAdress = IPAddress.Parse("127.0.0.1");
+
+            //TCP слухач
+            TcpListener server = new TcpListener(localAdress, port);
+            server.Start();
+            Console.WriteLine("Server start! Wait new client...");
+
+            while (true)
+            {
+                //Приймання клієнтів
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client online");
+
+                //Приймаємо данні від клієнту
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Client's name: {data}");
+
+                //Відповідаємо клієнту
+                string responce = $"Hello {data}";
+                byte[] responceBytes = Encoding.UTF8.GetBytes(responce);
+                stream.Write(responceBytes, 0, responceBytes.Length);
+
+                //Завершення
+                client.Close();
+                Console.WriteLine("Client offline");
+            }
+        }
+    }
+}
