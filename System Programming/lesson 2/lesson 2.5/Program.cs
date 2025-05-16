bb533f6e90cb37b3234d9c52753477b204558ee5
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-16 21:52:08 +0300

    Add files via upload

---
+++ System Programming/lesson 2/lesson 2.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lesson_2._4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Моніторинг завантаженинх процесів");
+            Console.WriteLine("Для завершення натисніть Ctrl + C...");
+            while (true)
+            {
+                Process[] processes = Process.GetProcesses();
+                Console.Clear();
+                foreach (Process p in processes.OrderBy(p => p.Id))
+                {
+                    Console.WriteLine($"{p.ProcessName,-40} {p.Id,-10}");
+                }
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
