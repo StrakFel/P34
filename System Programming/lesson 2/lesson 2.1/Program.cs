72f808e78f9b2340861a6d0a4c39f50208ea6594
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-16 09:41:14 +0300

    Add files via upload

---
+++ System Programming/lesson 2/lesson 2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace lesson_2._1
+{
+    /*
+             * Життєвий цикл процесу:
+             * Створення
+             * Очікування
+             * Виконання
+             * Призупинення
+             * Завершення 
+     */
+    internal class Program
+    {
+        static void Main()
+        {
+            Process process = Process.Start("notepad.exe");
+            Console.WriteLine($"Процес створенно! {process.Id}");
+
+            process.WaitForExit();
+            Console.WriteLine("Процес завершено!");
+        }
+    }
+}
---
+++ System Programming/lesson 2/lesson 2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace lesson_2._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("notepad.exe");
+            Process child = Process.Start(startInfo);
+
+            Console.WriteLine($"Дочірній процес запущено, PID: {child.Id}");
+        }
+    }
+}
