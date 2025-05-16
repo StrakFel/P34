9b92ee247ba4d9431213a41666caae87d8d80603
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-16 09:43:16 +0300

    Add files via upload

---
+++ System Programming/lesson 2/lesson 2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace lesson_2._3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                Console.WriteLine(process.Id + " " + process.ProcessName);
+            }
+        }
+    }
+}
