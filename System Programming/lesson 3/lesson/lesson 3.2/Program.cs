63cde40993c099f2254483d5427df2f1ac09ec83
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-18 19:33:27 +0300

    Add files via upload

---
+++ System Programming/lesson 3/lesson/lesson 3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lesson_3._2
+{
+    internal class Program
+    {
+        static bool running = true;
+        static void PrintNumbers()
+        {
+            while (running)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine($"Потік:{i}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Thread thread = new Thread(PrintNumbers);
+            thread.Start();
+
+            Console.ReadLine();
+            running = false;
+        }
+    }
+}
---
+++ System Programming/lesson 3/lesson/lesson 3.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lesson_3._3
+{
+    internal class Program
+    {
+        static bool paused = false;
+        static void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                while (paused)
+                {
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine($"Потік:{i}");
+                Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Thread thread = new Thread(PrintNumbers);
+            thread.Start();
+
+            Console.WriteLine("Натисніть Enter щоб призупинити потік...");
+            Console.ReadLine();
+            paused = true;
+
+            Console.WriteLine("Натисніть Enter щоб запустити потік...");
+            Console.ReadLine();
+            paused = false;
+        }
+    }
+}
