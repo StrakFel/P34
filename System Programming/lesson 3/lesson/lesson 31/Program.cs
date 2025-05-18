8369277535072cd2d42c0e2459e1a7639d5b0b5f
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-18 14:19:19 +0300

    Add files via upload

---
+++ System Programming/lesson 3/lesson/lesson 31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lesson_3._1
+{
+    internal class Program
+    {
+        static void PrintNumbers()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Потік:{i}");
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintNumbers);
+            thread.Start();
+
+            thread.Join();
+            Console.WriteLine("Головний потік працює...");
+        }
+    }
+}
