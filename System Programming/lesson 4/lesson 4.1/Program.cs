cfaba5362ed4b6136f9c3480face7742a6828db7
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-19 22:12:29 +0300

    Add files via upload

---
+++ System Programming/lesson 4/lesson 4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace lesson_4._1
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static object lockObject = new object();
+
+        static void Increment()
+        {
+            for (int i = 0; i < 1000; i++)
+                lock(lockObject) // lock Гарантує що тільки 1 потік буде змінювати counter
+                    counter++;
+        }
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Increment);
+            Thread thread2 = new Thread(Increment);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine($"Фінальне значення: {counter}");
+        }
+    }
+}
