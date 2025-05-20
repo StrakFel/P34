49394783af4a8ded9bd9351631201cba9f400523
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-20 16:42:09 +0300

    Add files via upload

---
+++ System Programming/lesson 5/lesson 5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson_5._2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List <int> {1, 2, 3, 4, 5};
+
+            Parallel.ForEach(numbers, number =>
+            {
+                Console.WriteLine($"Потік {Thread.CurrentThread.ManagedThreadId} обробляє число {number}");
+                Thread.Sleep(1000);
+            });
+        }
+    }
+}
