4ad842be46f19105e6324e5ad11628ca10b77342
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-20 16:46:52 +0300

    Add files via upload

---
+++ System Programming/lesson 5/lesson 5.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson_5._3
+{
+    internal class Program
+    {
+        static void Task1() { Thread.Sleep(1000); Console.WriteLine("Завадння 1 виконано"); }
+        static void Task2() { Thread.Sleep(1000); Console.WriteLine("Завадння 2 виконано"); }
+        static void Task3() { Thread.Sleep(1000); Console.WriteLine("Завадння 3 виконано"); }
+
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(Task1, Task2, Task3);
+        }
+    }
+}
