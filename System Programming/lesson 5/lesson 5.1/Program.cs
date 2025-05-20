a1c78c1a61f069bc041bac31f01d06d6cd4555a2
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-20 16:32:47 +0300

    Add files via upload

---
+++ System Programming/lesson 5/lesson 5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson_5._1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Parallel.For(1, 6, i =>
+            {
+                Console.WriteLine($"Потік {Thread.CurrentThread.ManagedThreadId} обробляє інтерацію {i}");
+                Thread.Sleep(1000);
+            });
+        }
+    }
+}
