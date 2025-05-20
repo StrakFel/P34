524d3c23856f85fffff463e4c5f12bd4be44efa7
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-20 11:05:51 +0300

    Add files via upload

---
+++ System Programming/lesson 4/lesson 4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace lesson_4._2
+{
+    internal class Program
+    {
+        static object lockObject = new object();
+
+        static void PrintMessage(string message)
+        {
+            Monitor.Enter(lockObject);
+            try
+            {
+                Console.WriteLine($"[Потік {Thread.CurrentThread.ManagedThreadId}] {message}");
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() => PrintMessage("Привіт!"));
+            Thread thread2 = new Thread(() => PrintMessage("Як ти?"));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+        }
+    }
+}
