0adc2d72405cb26ac1d5e188084ab81fb5356d75
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-20 11:16:15 +0300

    Add files via upload

---
+++ System Programming/lesson 4/lesson 4.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace lesson_4._3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            using (Mutex mutex = new Mutex(false, "MyUniaqeuApp"))
+            {
+                if (!mutex.WaitOne(TimeSpan.Zero,true))
+                {
+                    Console.WriteLine("Програма вже завантажена!");
+                }
+                Console.WriteLine("Програма вже завантажена! Натисніть на кнопку Enter...");
+            }
+        }
+    }
+}
