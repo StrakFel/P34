849bc882feb21505c5f4e96d5f585574042c2f66
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-20 16:57:12 +0300

    Add files via upload

---
+++ System Programming/lesson 5/lesson 5.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lesson_5._4
+{
+    internal class Program
+    {
+        static async Task DoWorkAsync()
+        {
+            Console.WriteLine("Початок роботи...");
+            await Task.Delay(1000);
+            Console.WriteLine("Робота завершена.");
+        }
+        static async Task Main(string[] args)
+        {
+            await DoWorkAsync();
+            Console.WriteLine("Основний потік не заблокований!");
+        }
+    }
+}
