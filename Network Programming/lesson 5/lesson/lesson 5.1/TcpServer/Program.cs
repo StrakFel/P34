154fffb6a674843807796b34533aa205a088eead
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-21 17:42:20 +0300

    Add files via upload

---
+++ Network Programming/lesson 5/lesson/lesson 5.1/TcpServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace TcpUnicastServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 8888);
+            Console.WriteLine("Server online");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[1024];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                Console.WriteLine($"Clietn messgae: {message}");
+
+                string response = "Response";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+
+                client.Close();
+            }
+        }
+    }
+}
---
+++ Network Programming/lesson 5/lesson/lesson 5.1/TcpUnicastClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpUnicastClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 8888);
+            NetworkStream stream = client.GetStream();
+
+            string message = "Hello server";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+            byte[] response = new byte[1024];
+            int bytes = stream.Read(response, 0, response.Length);
+            Console.WriteLine($"Response: {Encoding.UTF8.GetString(response, 0, response.Length)}");
+
+            client.Close();
+        }
+    }
+}
