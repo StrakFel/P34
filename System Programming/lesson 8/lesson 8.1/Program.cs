bad93abd230e35d12ec0b1600d6c0ff88f9ce85a
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-21 15:20:32 +0300

    Add files via upload

---
+++ System Programming/lesson 8/lesson 8.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace lesson_8._1
+{
+    internal class Program
+    {
+        static void DoWork(string text)
+        {
+            Console.WriteLine("Some one text: " + text);
+            Thread.Sleep(1000);
+        }
+        static void Main(string[] args)
+        {
+            Thread one = new Thread(() => DoWork("Високий пріоритет"));
+            Thread two = new Thread(() => DoWork("Низький пріоритет"));
+
+            one.Priority = ThreadPriority.Highest;
+            two.Priority = ThreadPriority.Lowest;
+
+            one.Start();
+            two.Start();
+
+            one.Join();
+            two.Join();
+
+            Console.WriteLine("End");
+        }
+    }
+}
