b94ec3a8b25b9406995c70caf0dbf303cd33bff6
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-21 17:11:20 +0300

    Add files via upload

---
+++ Network Programming/lesson 4/lesson/lesson 4.1/UdpClientApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpClientApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+
+            while (true)
+            {
+                Console.WriteLine("Enter your message");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                client.Send(data, data.Length, serverEndPoint);
+
+                IPEndPoint remoteEP = null;
+                byte[] responceBytes = client.Receive(ref remoteEP);
+                string responce = Encoding.UTF8.GetString(responceBytes);
+                Console.WriteLine("Server: " + responce);
+            }
+        }
+    }
+}
---
+++ Network Programming/lesson 4/lesson/lesson 4.1/UdpServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(5000);
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any,0);
+
+            Console.WriteLine("UDP Server is start...");
+
+            while (true)
+            {
+                byte[] data = server.Receive(ref clientEndPoint);
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine("input ", message);
+
+                string responce = "output data";
+                byte[] responceBytes = Encoding.UTF8.GetBytes(responce);
+                server.Send(responceBytes, responceBytes.Length, clientEndPoint);
+            }
+        }
+    }
+}
