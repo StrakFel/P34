9bc7b251be8523412df0356861ad07451b582a76
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-21 14:27:50 +0300

    Add files via upload

---
+++ System Programming/lesson 7/lesson 7.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathLibrary;
+
+namespace lesson_7._1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Add(2, 4));
+            Console.WriteLine(calculator.Multiply(2, 4));
+        }
+    }
+}
