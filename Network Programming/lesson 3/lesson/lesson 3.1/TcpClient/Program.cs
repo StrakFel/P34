901c8f754cf86281745d197448f4a451aa950af8
StrakFel <154830897+StrakFel@users.noreply.github.com>
2025-05-21 15:39:04 +0300

    Add files via upload

---
+++ Network Programming/lesson 3/lesson/lesson 3.1/TcpClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient("127.0.0.1", 5000);
+            //tcpClient.Connect();
+
+            NetworkStream stream = tcpClient.GetStream();
+
+            while (true)
+            {
+                Console.Write("Enter your message: ");
+                string message = Console.ReadLine();
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string responce = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Server responce: {responce}");
+            }
+        }
+    }
+}
---
+++ Network Programming/lesson 3/lesson/lesson 3.1/TcpServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace tcpServer
+{
+    internal class Program
+    {
+        static void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[256];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string data = Encoding.UTF8.GetString(buffer, 0 ,bytesRead);
+                Console.WriteLine($"Client writes: {data}");
+
+                string responce = "Received" + data;
+                byte[] responceBytes = Encoding.UTF8.GetBytes(responce);
+                stream.Write(responceBytes, 0, responceBytes.Length);
+            }
+            client.Close();
+        }
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Server status: Online");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("New client connection");
+                Thread thread = new Thread(HandleClient);
+                thread.Start(client);
+            }
+        }
+    }
+}
